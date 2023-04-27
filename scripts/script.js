1580e6f27ebff0684bf9e14d453d7eeb959c59b6
Natalya <cerea62@yarndex.ru>
2023-04-27 15:46:35 +0300

    Добавить попап новое место

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,45 @@
 // открытие и закрытие окна попапа
 //объявление переменных
-const popupElement = document.querySelector('.popup'); //окно попапа
+const popupProfileElement = document.querySelector('.popup_box_edit-profile'); //окно попапа редактирования профиля
+const popupNewPlaceElement = document.querySelector('.popup_box_new-place'); //окно попапа добавления нового места
 const editButtonProfileElement = document.querySelector('.profile__edit-button'); //кнопка редактирования
-const closeButtonPopupElement = popupElement.querySelector('.popup__close'); //кнопка закрытия
+const addButtonNewPlaceElement = document.querySelector('.profile__add-button'); //кнопка добавления нового места
+const closeButtonPopupProfilelement = popupProfileElement .querySelector('.popup__close'); //кнопка закрытия попапа редактирования профиля
+const closeButtonPopupNewPlacelement = popupNewPlaceElement.querySelector('.popup__close'); //кнопка закрытия окна добавления нового места
 const formElement = document.querySelector('.form');
 const nameInputElement = formElement.querySelector('.form__field_type_name');
 const jobInputElement = formElement.querySelector('.form__field_type_job');
 const profileNameElement = document.querySelector('.profile__name');
 const profileCaptionElement = document.querySelector('.profile__caption');
 
-function openPopup() { //функция открытия попапа
+function openPopupProfile() { //функция открытия попапа окна редактирования
     profileNameElement.value = nameInputElement.textContent; //записывает значение из профиля
     profileCaptionElement.value = jobInputElement.textContent; //в поля формы
-    popupElement.classList.add('popup_opened');
+    popupProfileElement.classList.add('popup_opened');
 };
 
-function closePopup() { //функция закрытия попапа
-    popupElement.classList.remove('popup_opened');
+function closePopupProfile() { //функция закрытия попапа окна редактирования
+    popupProfileElement.classList.remove('popup_opened');
+};
+
+function openPopupNewPlace() { //функция открытия попапа окна добавления места
+    popupNewPlaceElement.classList.add('popup_opened');
+};
+
+function closePopupNewPlace() {
+    popupNewPlaceElement.classList.remove('popup_opened');
 };
 
 function handleFormSubmit(evt) { //обработчик формы отправки, пока не работает
     evt.preventDefault();
     profileNameElement.textContent = nameInputElement.value
     profileCaptionElement.textContent = jobInputElement.value
-    closePopup();
+    closePopupProfile();
 };
 
-editButtonProfileElement.addEventListener('click', openPopup);
-closeButtonPopupElement.addEventListener('click', closePopup);
+editButtonProfileElement.addEventListener('click', openPopupProfile);
+closeButtonPopupProfilelement.addEventListener('click', closePopupProfile);
+
+addButtonNewPlaceElement.addEventListener('click', openPopupNewPlace);
+closeButtonPopupNewPlacelement .addEventListener('click', closePopupNewPlace);
 formElement.addEventListener('submit', handleFormSubmit);
